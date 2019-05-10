f124b94fca7025e849f70a53eb50429f90370b69
Tinshu Sasi <thetinshusasi@gmail.com>
2019-05-10 22:17:05 +0530

    commit temp

--- BondIssuanceHackFest.DLL/IRepositories/IQuorumNodeRepository.cs
+++ BondIssuanceHackFest.DLL/IRepositories/IQuorumNodeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BondIssuanceHackFest.DLL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BondIssuanceHackFest.DLL.IRepositories
 {
-    interface IQuorumNode
+    public interface IQuorumNodeRepository: IRepository<QuorumNode>
     {
     }
 }
--- BondIssuanceHackFest.DLL/Repositories/QuorumNodeRepository.cs
+++ BondIssuanceHackFest.DLL/Repositories/QuorumNodeRepository.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using BondIssuanceHackFest.DLL.IRepositories;
+using BondIssuanceHackFest.DLL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace BondIssuanceHackFest.DLL.Repositories
 {
-    class IQuorumNodeRepository
+    public class QuorumNodeRepository : Repository<QuorumNode>, IQuorumNodeRepository
     {
+        public QuorumNodeRepository(DbContext context) : base(context)
+        {
+        }
     }
 }
